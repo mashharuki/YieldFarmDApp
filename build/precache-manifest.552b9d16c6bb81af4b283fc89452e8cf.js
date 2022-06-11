354cc82b62859eb48c930285d396c8cbca1613ce
mashharuki <avp.104.106.107@ae.auone-net.jp>
2022-06-11 23:10:26 +0900

    ビルドした成果物を追加します。

---
+++ build/precache-manifest.552b9d16c6bb81af4b283fc89452e8cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cbcd5179ce69d2586308",
+    "url": "/static/css/main.f54ba22d.chunk.css"
+  },
+  {
+    "revision": "cbcd5179ce69d2586308",
+    "url": "/static/js/main.cbcd5179.chunk.js"
+  },
+  {
+    "revision": "537bf3cc80995352a0e1",
+    "url": "/static/css/1.58aee81a.chunk.css"
+  },
+  {
+    "revision": "537bf3cc80995352a0e1",
+    "url": "/static/js/1.537bf3cc.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "696d611b81ef11e83b173a0c0c91318d",
+    "url": "/index.html"
+  }
+];
